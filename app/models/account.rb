1ea0dd939b91bca55f55932437aae5be6ea8de5f
Ian Unruh <ianunruh@gmail.com>
2014-04-03 01:51:37 -0500

    Added convenience methods to Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,14 @@ class Account < ActiveRecord::Base
   has_many :addresses
   has_many :orders
   has_many :transfers
+
+  def hold(amount)
+    self.balance -= amount
+    self.held_balance += amount
+  end
+
+  def unhold(amount)
+    self.balance += amount
+    self.held_balance -= amount
+  end
 end
