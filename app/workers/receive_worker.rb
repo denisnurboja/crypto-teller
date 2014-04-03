53f8fcb3b0a6f713bb343682374c5419e6212afc
Ian Unruh <ianunruh@gmail.com>
2014-04-03 02:28:29 -0500

    Added fluent logging to ReceiveWorker

--- app/workers/receive_worker.rb
+++ app/workers/receive_worker.rb
@@ -12,11 +12,11 @@ class ReceiveWorker
     transactions.each do |tx|
       address = Address.find_by(address: tx.address)
       if address
-        logger.info "Processing deposit to #{tx.address} for #{tx.amount} #{tx.currency}"
+        log('sell_order.received', tx_id: tx.txrid, address: tx.address, currency: tx.currency, amount: tx.amount)
         normalize_exchange_account(tx)
         process(tx, address.account)
       else
-        logger.debug "Ignoring deposit #{tx.trxid} to #{tx.address}"
+        log('sell_order.ignored', tx_id: tx.txrid, address: tx.address)
       end
     end
   end
@@ -25,6 +25,8 @@ class ReceiveWorker
 
   def process(tx, account)
     amount = CryptoTeller.price_service.to_usd(tx.currency, tx.amount.to_f)
+
+    log('sell_order.valued', currency: tx.currency, amount: amount)
 
     order = account.orders.create!(
       direction: 'sell',
@@ -38,11 +40,18 @@ class ReceiveWorker
 
     account.balance += amount
     account.save!
+
+    log('sell_order.credited', account_id: account.id, amount: amount)
   end
 
   def normalize_exchange_account(tx)
     unless tx.currency == 'BTC'
+      log('sell_order.instant_sell', currency: tx.currency, amount: tx.amount)
       CryptoTeller.trade_service.instant_sell(tx.currency, 'BTC', tx.amount)
     end
+  end
+
+  def log(*args)
+    Fluent::Logger.post(*args)
   end
 end
